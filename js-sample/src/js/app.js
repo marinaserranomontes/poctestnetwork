cf1a8c47b07a5af876a3504b8722cd49d6a3bc0e
Michael Leaney <mleaney@tokbox.com>
2015-06-15 10:39:18 +1000

    Update JS sample to match behaviour of Android

--- js-sample/src/js/app.js
+++ js-sample/src/js/app.js
@@ -11,9 +11,47 @@ var publisherEl = document.createElement('div'),
     subscriberEl = document.createElement('div'),
     session,
     publisher,
+    subscriber,
     statusContainerEl,
     statusMessageEl,
     statusIconEl;
+
+var testStreamingCapability = function(subscriber, callback) {
+  performQualityTest({subscriber: subscriber, timeout: TEST_TIMEOUT_MS}, function(error, results) {
+    console.log('Test concluded', results);
+
+    if (results.video.bytesPerSecond > 150000 && results.video.packetLossRatioPerSecond < 0.03 && results.audio.packetLossRatioPerSecond < 0.03) {
+      return callback(false, {
+        text: "You're all set!",
+        icon: 'assets/icon_tick.svg'
+      });
+    }
+
+    if ((results.video.packetLossRatioPerSecond + results.audio.packetLossRatioPerSecond) / 2 < 0.05) {
+      return callback(false, {
+        text: 'Your bandwidth can support audio only',
+        icon: 'assets/icon_warning.svg'
+      });
+    }
+
+    // try audio only to see if it reduces the packet loss
+    statusMessageEl.innerText = 'Trying audio only';
+    publisher.publishVideo(false);
+
+    performQualityTest({subscriber: subscriber, timeout: 5000}, function(error, results) {
+      if (results.audio.packetLossRatioPerSecond < 0.05) {
+        return callback(false, {
+          text: 'Your bandwidth can support audio only',
+          icon: 'assets/icon_warning.svg'
+        });
+      }
+      return callback(false, {
+        text: 'Your bandwidth is too low for audio',
+        icon: 'assets/icon_error.svg'
+      });
+    });
+  });
+};
 
 var callbacks = {
   onInitPublisher: function onInitPublisher(error) {
@@ -30,7 +68,7 @@ var callbacks = {
       return;
     }
     statusMessageEl.innerText = 'Subscribing to video';
-    session.subscribe(
+    subscriber = session.subscribe(
       publisher.stream,
       subscriberEl,
       {
@@ -40,6 +78,10 @@ var callbacks = {
       callbacks.onSubscribe
     );
   },
+  cleanup: function() {
+    session.unsubscribe(subscriber);
+    session.unpublish(publisher);
+  },
   onSubscribe: function onSubscribe(error, subscribe) {
     if (error) {
       statusMessageEl.innerText = 'Could not subscribe to video';
@@ -48,32 +90,10 @@ var callbacks = {
 
     statusMessageEl.innerText = 'Checking your available bandwidth';
 
-    performQualityTest({subscriber: subscribe, timeout: TEST_TIMEOUT_MS}, function(error, results) {
-      // here we can decide what to do.
-      console.log('Test concluded', results);
-
-      if (results.video.bytesPerSecond > 150000 && results.video.packetLossRatioPerSecond < 0.03 && results.audio.packetLossRatioPerSecond < 0.03) {
-        statusMessageEl.innerText = 'You\'re all set!';
-        statusIconEl.src = 'assets/icon_tick.svg';
-      } else if (results.video.bytesPerSecond > 5000 && results.video.packetLossRatioPerSecond < 0.03) {
-        statusMessageEl.innerText = 'Your bandwidth is too low for video';
-        statusIconEl.src = 'assets/icon_warning.svg';
-      } else {
-
-        // maybe try audio only
-        statusMessageEl.innerText = 'Trying audio only';
-        publisher.publishVideo(false);
-
-        performQualityTest({subscriber: subscribe, timeout: 5000}, function(error, results) {
-          if (results.audio.bytesPerSecond > 5000 && results.audio.packetLossRatioPerSecond < 0.03) {
-            statusMessageEl.innerText = 'Your bandwidth can support audio only';
-            statusIconEl.src = 'assets/icon_warning.svg';
-          } else {
-            statusMessageEl.innerText = 'Your bandwidth is too low for audio';
-            statusIconEl.src = 'assets/icon_error.svg';
-          }
-        });
-      }
+    testStreamingCapability(subscribe, function(error, message) {
+      statusMessageEl.innerText = message.text;
+      statusIconEl.src = message.icon;
+      callbacks.cleanup();
     });
   },
   onConnect: function onConnect(error) {
@@ -87,17 +107,17 @@ compositeOfCallbacks(
   callbacks,
   ['onInitPublisher', 'onConnect'],
   function(error) {
-    statusMessageEl.innerText = 'Publishing video';
     if (error) {
-      // handle getUserMedia + session connect errors here
-      // statusMessageEl.innerText = 'Could not publish video';
       return;
     }
+    statusMessageEl.innerText = 'Publishing video';
     session.publish(publisher, callbacks.onPublish);
   }
 );
 
 publisher = OT.initPublisher(publisherEl, {
+  // for other resoultions you may need to adjust the bandwidth conditions in
+  // testStreamingCapability()
   resolution: '1280x720'
 }, callbacks.onInitPublisher);
 
